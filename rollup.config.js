bb8607c3e4b3c1c989dea36b06350edce31577ca
Aleksey Makhankov <aleksey.makhankov@gmail.com>
2019-04-19 14:53:24 +0300

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,25 @@
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { uglify } from "rollup-plugin-uglify";
 
 module.exports = {
   input: 'src/index.js',
   output: {
     file: 'dist/index.js',
-    format: 'cjs'
+    format: 'cjs',
+    exports: 'named',
   },
   plugins: [
+    peerDepsExternal(),
+    resolve({
+      extensions: ['js', 'jsx'],
+      preferBuiltins: false,
+    }),
     babel({
       exclude: 'node_modules/**',
-      presets: ['@babel/env', '@babel/preset-react']
+      presets: [['@babel/env', { modules: false }], '@babel/preset-react']
     }),
     commonjs(),
     uglify(),
