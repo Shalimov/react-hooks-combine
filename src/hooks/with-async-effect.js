22728290be76718d51b523b63e206dc382281e2a
Aleksey Makhankov <aleksey.makhankov@gmail.com>
2019-04-23 12:57:48 +0300

    update husky config

--- src/hooks/with-async-effect.js
+++ src/hooks/with-async-effect.js
@@ -3,15 +3,15 @@ import { useEffect, useState } from 'react'
 import { getDeps, isFunction, isPromiseLike, getInternalCtor } from '../utils'
 
 export const withAsyncEffect = (asyncCallback, deps, unmountCallback) => (state, props) => {
-  if (!asyncCallback || !isFunction(asyncCallback)) {
+  if (!isFunction(asyncCallback)) {
     throw Error(`withAsyncEffect expects function, got a: ${typeof asyncCallback}`)
   }
 
   const [data, setData] = useState({ loading: true, data: null })
 
   useEffect(() => {
-
     setData({ loading: true })
+
     const promise = asyncCallback(state, props)
 
     if (!isPromiseLike(promise)) {
