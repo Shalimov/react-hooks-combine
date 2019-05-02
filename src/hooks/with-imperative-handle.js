8e0be4e8122a8b2d533c7b4e93189dea5d9064a5
Aleksey Makhankov <aleksey.makhankov@gmail.com>
2019-05-02 20:20:09 +0300

    update getDeps props

--- src/hooks/with-imperative-handle.js
+++ src/hooks/with-imperative-handle.js
@@ -3,6 +3,6 @@ import { useImperativeHandle } from 'react'
 import { getDeps } from '../utils'
 
 export const withImperativeHandle = (ref, createHandler, dependencies) => (state, props) => {
-  const deps = getDeps(props, dependencies)
+  const deps = getDeps({ ...state, ...props }, dependencies)
   useImperativeHandle(ref, () => createHandler(state, props), deps)
 }
--- src/hooks/with-layout-effect.js
+++ src/hooks/with-layout-effect.js
@@ -3,6 +3,6 @@ import { useLayoutEffect } from 'react'
 import { getDeps } from '../utils'
 
 export const withLayoutEffect = (fn, dependencies) => (state, props) => {
-  const deps = getDeps(props, dependencies)
+  const deps = getDeps({ ...state, ...props }, dependencies)
   useLayoutEffect(() => fn(state, props), deps)
 }
