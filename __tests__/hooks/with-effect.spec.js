a0feee4214c09fc29aac388763fb4cc2fed2d3de
Shalimov <Shalimich@gmail.com>
2019-06-21 19:44:52 +0300

    Update withEffect tests

--- __tests__/hooks/with-effect.spec.js
+++ __tests__/hooks/with-effect.spec.js
@@ -34,4 +34,32 @@ describe('With Effect hook', () => {
     expect(sideEffects[1]).toBe(false)
     expect(sideEffects[2]).toBe(false)
   })
+
+  test('should call side effects when mounting, updating and unmounting', () => {
+    const sideEffects = { currProps: null, prevProps: null }
+
+    const { rerender } = renderHook(
+      ({ id }) => withEffect((state, props, { props: prevProps }) => {
+
+        sideEffects.currProps = props
+        sideEffects.prevProps = prevProps
+
+      }, ['id'])({}, { id }),
+      { initialProps: { id: 1 } }
+    )
+
+
+    expect(sideEffects.currProps.id).toBe(1)
+    expect(sideEffects.prevProps.id).toBe(1)
+
+    rerender({ id: 2 })
+
+    expect(sideEffects.currProps.id).toBe(2)
+    expect(sideEffects.prevProps.id).toBe(1)
+
+    rerender({ id: 3 })
+
+    expect(sideEffects.currProps.id).toBe(3)
+    expect(sideEffects.prevProps.id).toBe(2)
+  })
 })
